5e0940c6487f212d00744da25e191fe481f37b43
Chetna Tuli <chetnatuli@outlook.com>
2021-03-30 10:41:08 +0100

    Added score method and sum logic of all the frames

--- BowlingGame.Tests/GameTests.cs
+++ BowlingGame.Tests/GameTests.cs
@@ -16,5 +16,44 @@ namespace BowlingGame.Tests
         {
             Assert.Equal(10, game.Frames.Length);
         }
+
+        [Fact]
+        public void Roll_ForAGivenThrow_ShouldAddPinsInAppropriateFrameIndex()
+        {
+            game.Roll(1);
+            game.Roll(1);
+
+            Assert.Equal(2, game.Frames[0].FrameScore);
+        }
+
+        [Fact]
+        public void Roll_WhenFrameIsCompleted_ShouldIncrementFrameIndex()
+        {
+            game.Roll(1);
+            game.Roll(1); //First Frame
+
+            game.Roll(5); //Second Frame
+            game.Roll(5);
+
+            Assert.Equal(2, game.Frames[0].FrameScore);
+            Assert.Equal(10, game.Frames[1].FrameScore);
+            Assert.True(game.Frames[1].IsSpare);
+        }
+
+        [Fact]
+        public void Score_ShouldReturnSumOfAllTheScoreOfFrames()
+        {
+            //Arrange
+            game.Roll(1);
+            game.Roll(1); //First Frame
+
+            game.Roll(5); //Second Frame
+            game.Roll(5);
+
+            //Act
+            var totalScore =  game.Score();
+
+            Assert.Equal(12, totalScore);
+        }
     }
 }
--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -1,21 +1,56 @@
-﻿using System;
-
-namespace BowlingGame
+﻿namespace BowlingGame
 {
     public class Game
     {
         public Frame[] Frames;
-
-        const int MaxFrame = 10;
+        private int frameIndex;
+        private const int MaxFrame = 10;
 
         public Game()
         {
             Frames = new Frame[MaxFrame];
         }
 
+        private Frame GetFrame()
+        {
+            var frame = Frames[frameIndex];
+
+            if (frame == null)
+            {
+                frame = frameIndex == MaxFrame - 1 ? new TenthFrame() : new Frame();
+
+                Frames[frameIndex] = frame;
+            }
+
+            return frame;
+        }
+
         public void Roll(int pins)
         {
-            throw new NotImplementedException();
+            var frame = GetFrame();
+
+            frame.Add(pins);
+
+            if (frame.IsFrameCompleted && frameIndex < MaxFrame - 1)
+            {
+                frameIndex++;
+            }
         }
+
+        public int Score()
+        {
+            int score = 0;
+            int i = 0;
+            foreach (var frame in Frames)
+            {
+                if (frame == null) continue;
+
+                score += frame.FrameScore;
+                i++;
+            }
+
+            return score;
+        }
+
     }
 }
